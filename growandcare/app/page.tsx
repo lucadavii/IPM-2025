19bbba6cea2f5c058c5ef4b3c7275e2e5eb12087
Luca Davì <lucadavi@MacBook-Air-di-Luca.local>
2025-09-27 17:38:09 +0100

    Project bootstrap with pnpm shadcn and tailwind

---
+++ growandcare/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center bg-background">
+      <Button >Click me</Button>
+    </div>
+  );
+}
