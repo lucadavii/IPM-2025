746cbeec558a95ec1e6e05f7973392a00636a01e
Luca Davì <lucadavi@MacBook-Air-di-Luca.local>
2025-11-12 18:48:36 +0000

    fix navigation bar issues

--- growandcare/app/page.tsx
+++ growandcare/app/page.tsx
@@ -1,19 +1,37 @@
 import { Button } from "@/components/ui/button";
 import { supabase } from "@/lib/supabase";
 import { redirect } from "next/navigation";
+import Image from "next/image";
 
 export default async function Home() {
-  const { data,error } = await supabase.from('Test').select('*');
-  if (error) {
-    console.error("Error fetching members:", error);
-    return <div>Error loading members</div>;
-  }
+  // const { data,error } = await supabase.from('Test').select('*');
+  // if (error) {
+  //   console.error("Error fetching members:", error);
+  //   return <div>Error loading members</div>;
+  // }
   //console.log(data);
   return (
     <>
     {/* redirect to /about */}
-    {redirect("/about")}
-    <div className="flex min-h-screen flex-col items-center justify-center bg-background">
+    {/*{redirect("/about")}*/}
+
+    <div className="flex min-h-screen flex-col items-center justify-start bg-background px-6 py-24">
+      
+      <Image
+        src="/favicon.ico"
+        alt="Grow&Care Logo"
+        width={150}
+        height={150}
+        className="mb-12"
+      />
+      <h1 className="scroll-m-20 text-center text-3xl lg:text-5xl font-extrabold tracking-tight text-balance">Welcome to Grow&amp;Care</h1>
+      <p className="mt-6 text-center text-lg max-w-2xl text-muted-foreground">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sequi maiores necessitatibus officia, ut iure culpa, eveniet eos corporis exercitationem optio voluptate assumenda suscipit rem quod facilis consectetur, impedit quasi.
+      </p>
+    </div>
+
+
+    {/* <div className="flex min-h-screen flex-col items-center justify-center bg-background">
       <Button>Click me</Button>
     </div>
     <main className="mx-auto max-w-2xl p-6">
@@ -25,6 +43,7 @@ export default async function Home() {
             </li>
           ))}
         </ul>
-      </main></>
+      </main></> */}
+    </>
   );
 }
--- growandcare/components/ui/navmenu.tsx
+++ growandcare/components/ui/navmenu.tsx
@@ -3,17 +3,61 @@ import {
     NavigationMenu,
     NavigationMenuList,
     NavigationMenuItem,
-    NavigationMenuLink
+    NavigationMenuLink,
+    NavigationMenuContent,
+    NavigationMenuTrigger,
+    NavigationMenuViewport
 } from "@/components/ui/navigation-menu";
+import Image from "next/image";
 
 export function NavMenu() {
     return (
-        <NavigationMenu className="w-full border-b bg-white/50 backdrop-blur-sm">
+        <NavigationMenu viewport={false}>
             <NavigationMenuList>
                 <NavigationMenuItem>
                     <NavigationMenuLink asChild>
                         <Link href="/" className="px-3 py-2">
-                            Home
+                            <Image src="/favicon.ico" alt="Grow&Care Logo" width={24} height={24} />
+                        </Link>
+                    </NavigationMenuLink>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <NavigationMenuLink asChild>
+                        <Link href="/tips" className="px-3 py-2">
+                            Tips
+                        </Link>
+                    </NavigationMenuLink>
+                </NavigationMenuItem>
+                <NavigationMenuItem >
+                    <NavigationMenuTrigger> Activities </NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                        <ul className="grid w-[320px] gap-2">
+                            <li>
+                                <NavigationMenuLink asChild>
+                                    <Link href="/about" className="block rounded-md px-3 py-2 hover:bg-accent hover:text-accent-foreground">
+                                        My Activities
+                                    </Link>
+                                </NavigationMenuLink>
+                                <NavigationMenuLink asChild>
+                                    <Link href="/about" className="block rounded-md px-3 py-2 hover:bg-accent hover:text-accent-foreground">
+                                        Browse Activities
+                                    </Link>
+                                </NavigationMenuLink>
+                            </li>
+                        </ul>
+                    </NavigationMenuContent>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <NavigationMenuLink asChild>
+                        <Link href="/about" className="px-3 py-2">
+                            Events
+                        </Link>
+                    </NavigationMenuLink>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <NavigationMenuLink asChild>
+                        <Link href="/about" className="px-3 py-2">
+                            Child Mode
                         </Link>
                     </NavigationMenuLink>
                 </NavigationMenuItem>
@@ -21,6 +65,13 @@ export function NavMenu() {
                     <NavigationMenuLink asChild>
                         <Link href="/about" className="px-3 py-2">
                             About
+                        </Link>
+                    </NavigationMenuLink>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <NavigationMenuLink asChild>
+                        <Link href="/profile" className="px-3 py-2">
+                            <Image src="/developer.png" alt="Developers" width={24} height={24} />
                         </Link>
                     </NavigationMenuLink>
                 </NavigationMenuItem>
